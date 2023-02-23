7cbcff7019073a40e6988dca08f220d0b3ee347e
SE140816\FLAMINGO-PC <anonymouszingme@gmail.com>
2023-02-23 20:09:23 +0700

    disable auto gen from Order

--- src/main/java/com/tfs/demo/tfs_crud_demo/entity/Order.java
+++ src/main/java/com/tfs/demo/tfs_crud_demo/entity/Order.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
